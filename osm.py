0446e426a875697d7f454072ec5e4b51e3fd5e57
John Iannandrea <jiannandrea@gmail.com>
2016-06-14 16:14:36 -0400

    Inital commit

---
+++ osm.py
@@ -0,0 +1,14 @@
+# Open Sound Mixer
+# written by John Iannandrea
+
+from install import *
+pipimport("sounddevice", "sounddevice>=0.3.3")
+
+def main():
+    print("Starting osm")
+
+    
+
+
+if __name__ == "__main__":
+    main()
