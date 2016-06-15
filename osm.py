0fd0b170549a5d39927eb1548272d030298700c2
John Iannandrea <jiannandrea@gmail.com>
2016-06-15 16:21:24 -0400

    Added output devices to ui

--- osm.py
+++ osm.py
@@ -17,7 +17,7 @@ def main():
     sd.default.samplerate = 44100
     #sd.default.device = "digital output"
     dc = DeviceController()
-    dc.setOutputDevice(9)
+    dc.setOutputDevice(10)
     device = dc
 
     # ui
@@ -43,6 +43,7 @@ class ui(tk.Frame):
         tk.Frame.__init__(self, master)
         self.device = device
         self.audioDevices = []
+        self.outputDevices = []
 
         #self.pack()
         self.createFrames()
@@ -51,8 +52,11 @@ class ui(tk.Frame):
         #threading.Thread(target=self.statusBarUpdate).start()
 
     def createFrames(self):
+        # canvas hack for scrollbar
+        #self.leftFrame = tk.Frame(self).pack(side="left")
+        #self.rightFrame = tk.Frame(self).pack(side="right", anchor="e")
         self.devicesFrame = tk.Frame(self, relief='sunken').grid(row=0)
-        self.bottomFrame = tk.Frame(self).grid(row=1)
+        #self.bottomFrame = tk.Frame(self).grid(row=1)
 
     def createWidgets(self):
 
@@ -66,6 +70,9 @@ class ui(tk.Frame):
         self.fileMenu.add_separator()
         self.fileMenu.add_command(label="Exit", command=self.quit)
         self.topBar.add_cascade(label="File", menu=self.fileMenu)
+
+        # output
+        self.addOutput(self.device.outputDevice)
 
         # status bar
         #self.statusBar = tk.Label(self.bottomFrame, text="Initial load", bd=1, relief=tk.SUNKEN, anchor=tk.E)
@@ -101,10 +108,25 @@ class ui(tk.Frame):
         ad = uiAudioDevice(master=self.devicesFrame, device=self.device)
         self.audioDevices.append(ad)
         ad.grid(row=0, column=len(self.audioDevices), sticky="w")
+        self.positionOut()
+
+    def addOutput(self, device):
+        ad = uiAudioDevice(master=self.devicesFrame, device=self.device, audioDevice=device)
+        self.outputDevices.append(ad)
+        self.positionOut()
+
+    def positionOut(self):
+        pos = 1
+        for outdev in self.outputDevices:
+            outdev.grid(row=0, column=len(self.audioDevices) + pos)
+            pos+=1
 
 class uiAudioDevice(tk.Frame):
     def __init__(self, master=None, audioDevice=None, device=None):
         tk.Frame.__init__(self, master, width=100, height=300)
+
+        self.volumeSize = 250
+
         #self.pack(side="left")
         self.audioDevice = audioDevice
         self.device = device
@@ -118,29 +140,36 @@ class uiAudioDevice(tk.Frame):
         #self.title.pack(side="left")
 
         # visual volume stuff
-        self.volume = tk.Canvas(self, width=50, height=300)
+        self.volume = tk.Canvas(self, width=50, height=self.volumeSize)
         self.volume.grid(row=1, column=0)
-        self.volume.create_rectangle(2, 0, 26, 300, fill="#d3d3d3")
-        self.volume.create_rectangle(26, 0, 50, 300, fill="#d3d3d3")
-        self.leftChannel = self.volume.create_rectangle(2, 200, 26, 300, fill="#66ff00")
-        self.rightChannel = self.volume.create_rectangle(26, 200, 50, 300, fill="#66ff00")
+        self.volume.create_rectangle(2, 0, 26, self.volumeSize, fill="#d3d3d3")
+        self.volume.create_rectangle(26, 0, 50, self.volumeSize, fill="#d3d3d3")
+        self.leftChannel = self.volume.create_rectangle(2, 200, 26, self.volumeSize, fill="#66ff00")
+        self.rightChannel = self.volume.create_rectangle(26, 200, 50, self.volumeSize, fill="#66ff00")
+
+        self.volumeScale = tk.Scale(self, from_=100, to=0, orient="vertical")
+        self.volumeScale.grid(row=1, column=1, sticky="w")
         threading.Thread(target=self.getVolume).start()
 
-        self.volumeScale = tk.Scale(self, from_=100, to=0, orient="vertical").grid(row=1, column=1, sticky="w")
+        self.selectDevice = tk.OptionMenu(self, "Empty", tuple(self.device.deviceList))
+        self.selectDevice.grid(row=2, columnspan=2, sticky="ew")
 
         self.toggle = tk.Button(self, text="Enable/Disable", fg="black", command=self.toggleDevice)
-        self.toggle.grid(row=2, columnspan=2)
+        self.toggle.grid(row=3, columnspan=2)
         #self.toggle.pack(fill=tk.X)
+
+        if (self.audioDevice != None):
+            self.title.config(text=self.audioDevice.name)
 
     def toggleDevice(self):
         self.audioDevice.active = not self.audioDevice.active
 
     def getVolume(self):
-        if (self.audioDevice != None):
+        if (self.audioDevice != None and self.audioDevice.getType() != "output"):
             self.audioDevice.volume = self.volumeScale.get() * 0.01
             avg = self.audioDevice.getDeviceAvg()
-            self.leftChannel = self.volume.create_rectangle(avg + 2, avg, 26, 300, fill="#66ff00")
-            self.rightChannel = self.volume.create_rectangle(avg + 26, avg, 50, 300, fill="#66ff00")
+            self.leftChannel = self.volume.create_rectangle(avg + 2, avg, 26, self.volumeSize, fill="#66ff00")
+            self.rightChannel = self.volume.create_rectangle(avg + 26, avg, 50, self.volumeSize, fill="#66ff00")
 
     def setDevice(self, id):
         devs = self.device.getDevice(id)
